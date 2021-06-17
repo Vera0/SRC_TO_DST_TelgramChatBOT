0d1ca071d39df09334e92efe47e2032e170d2d70
Nikita <31665239+Vera0@users.noreply.github.com>
2021-06-17 13:45:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.types import ContentType
 
 from config import API_TOKEN
 from aiogram import Bot, Dispatcher, executor, types
-from config import IVCGB_CHAT_ID, MOMIAC_CHAT_ID, TRIGGER_WORDS
+from config import DST_CHAT_ID, SRC_CHAT_ID, TRIGGER_WORDS
 
 
 logging.basicConfig(level=logging.INFO)
@@ -16,22 +16,22 @@ dp = Dispatcher(bot)
 
 @dp.message_handler()
 async def message_from_momiac(message: types.Message):
-    if message.chat.id == MOMIAC_CHAT_ID:
+    if message.chat.id == SRC_CHAT_ID:
         for i in TRIGGER_WORDS:
             if i in message.text.lower():
-                await bot.forward_message(IVCGB_CHAT_ID, message.chat.id, message.message_id)
+                await bot.forward_message(DST_CHAT_ID, message.chat.id, message.message_id)
                 logging.info(f"{message.date} New message from {message.from_user.full_name}")
 
 
 @dp.message_handler(content_types=ContentType.DOCUMENT)
 async def message_with_doc(document: types.Document):
-    if document.chat.id == MOMIAC_CHAT_ID:
+    if document.chat.id == SRC_CHAT_ID:
         binary_doc = await bot.download_file_by_id(document.document.file_id)
         xl_file = pandas.read_excel(binary_doc)
         for i in TRIGGER_WORDS:
             if i in str(document.caption).lower() or \
-                    "ивантеевская центральная городская больница" in xl_file.to_string().lower():
-                await bot.forward_message(IVCGB_CHAT_ID, document.chat.id, document.message_id)
+                    "Another_trigger_words" in xl_file.to_string().lower():
+                await bot.forward_message(DST_CHAT_ID, document.chat.id, document.message_id)
                 logging.info(f"{document.date} New message with document from {document.from_user.full_name}")
             break
 
